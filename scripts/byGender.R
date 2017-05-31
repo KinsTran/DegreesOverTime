3e3f64d824edcf5252ea40bc187943795e84a913
Kinsley <kins.tran@comcast.net>
2017-05-31 03:58:40 -0700

    Almost finished with chart: Need to assign variables. As of currently not working, will break down whole chart if trying to run. Comment out the relevent sections in UI and Server if attempting to work after pull

--- scripts/byGender.R
+++ scripts/byGender.R
@@ -1,4 +1,28 @@
 # Read in data
-males.data <- read.csv("data/Bach_Degrees_Males.csv", header = TRUE, stringsAsFactors = FALSE)
-females.data <- read.csv("data/Bach_Degrees_Females.csv", header = TRUE, stringsAsFactors = FALSE)
+males.data <- read.csv("./data/Bach_Degrees_Males.csv", header = TRUE, stringsAsFactors = FALSE)
+females.data <- read.csv("./data/Bach_Degrees_Females.csv", header = TRUE, stringsAsFactors = FALSE)
+colnames(males.data) <- gsub("\\.", " ", colnames(males.data))
+colnames(males.data) <- gsub("X", "", colnames(males.data))
+colnames(females.data) <- gsub("\\.", " ", colnames(females.data))
+colnames(females.data) <- gsub("X", "", colnames(females.data))
+plot.data <- c()
 
+GenderChart <- function(gender) { # https://stackoverflow.com/questions/1169248/r-function-for-testing-if-a-vector-contains-a-given-element
+  if(is.element("F1", input$genders)) {
+    plot.data <- c(females.data[2:37, "2013 14 Total"], "F1")
+  }
+  if(is.element("M1", input$genders)) {
+    plot.data <-c(plot.data, males.data[2:37, "2013 14 Total"], "M1")
+  } 
+  if(is.element("F2", input$genders)){
+    plot.data <-c(plot.data, females.data[2:37, "2014 15 Total"], "F2")
+  }
+  if(is.element("M2", input$genders)) {
+    plot.data <-c(plot.data, males.data[2:37, "2014 15 Total"], "M2")
+  }
+  
+  
+  
+  p <- ggplot()
+  return(p)
+}
--- server.R
+++ server.R
@@ -2,18 +2,20 @@
 library(dplyr)
 library(shiny)
 library(plotly)
+library(ggplot2)
 
 source('./scripts/byrace.R')
+source('./scripts/byGender.R')
 
 shinyServer(function(input, output) { 
   output$race <- renderPlotly({
     return(BuildGraph(input$year))
   })
   
-  #output$gender <- renderPlot({
-  #  return(genderPlot())
-  #})
-  #   
+  output$gender <- renderPlot({
+    return(GenderChart(input$genders))
+  })
+     
   # output$degrees 
   
 })
